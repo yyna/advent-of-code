86ca2dfaa6af1e255c71f2725edea1be411b27f0
Jungin Kwon <jungin.kwon1@gmail.com>
2021-07-07 19:26:42 +0900

    7/7 피드백 받은 부분 수정 중

--- src/day_1.clj
+++ src/day_1.clj
@@ -1,40 +1,53 @@
-(ns day_1)
+(ns day_1 (:require [clojure.java.io :as io]))
 
 (defn parse [input]
   "multiline string input 을 정수 sequence 로 변경해 return 하는 function"
   (->> (clojure.string/split-lines input)
        (map #(Integer/parseInt %))))
 
+(io/resource "day_1_input")
+
+(slurp (io/resource "day_1_input"))
+
 (defn sum [data]
   "number sequence input 의 값을 모두 더한 값을 return 하는 function"
   (apply + data))
 
-;[part 1]
-;주어진 입력의 모든 숫자를 더하시오.
-;
-;예) +10 -2 -5 +1 이 (입력일 경우 4를 출력
+;;[part 1]
+;;주어진 입력의 모든 숫자를 더하시오.
+;;
+;;예) +10 -2 -5 +1 이 (입력일 경우 4를 출력
+(defn solve-part-1 [input]
+  (->> input
+       parse
+       sum))
 (comment
   ;; slurp -> IO
   ;; IO 는 부수효과(side-effect)라고 합니다.
-  (->> (slurp "./src/day_1_input")
-       (parse)
-       (sum)))
+  (solve-part-1 input))
 
-(defn find_the_first_duplicate
+;;
+(defn find-the-first-duplicate
   "숫자 sequence 를 input 으로 받아 누적 합의 값이 처음으로 두번 나오는 숫자를 return 하는 function"
-  ([list] (find_the_first_duplicate list 0 #{}))
-  ([list sum count_set]
-   (let [current_value (first list) current_sum (+ current_value sum)]
-     (if (count_set current_sum)
-       current_sum
-       (recur (rest list) current_sum (conj count_set current_sum))))))
+  ([list] (find-the-first-duplicate list 0 #{}))
+  ([list sum count-set]
+   (let [current-value (first list)
+         current-sum (+ current-value sum)]
+     (if (count-set current-sum)
+       current-sum
+       (recur (rest list) current-sum (conj count-set current-sum))))))
 
-;[part 2]
-;주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
-;
-;예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임. 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
+;(1) loop - 초기값
+;(2) reduce 로 바꿔보기 (reduced 로 중간에 탈출하기)
+
+;slurp 상대경로 X
+
+;;[part 2]
+;;주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
+;;
+;;예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임. 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 (comment
-  (->> (slurp "./src/day_1_input")
-       (parse)
-       (cycle)
-       (find_the_first_duplicate)))
+  (->> input
+       parse
+       cycle
+       find-the-first-duplicate))
--- src/day_2.clj
+++ src/day_2.clj
@@ -1,2 +1,48 @@
 (ns day_2)
 
+(defn parse [input]
+  "multiline string input 을 string sequence 로 변경해 return 하는 function"
+  (clojure.string/split-lines input))
+
+;read input
+(def input (->> (slurp "./src/day_2_input")
+                (parse)))
+
+;; parse 된 자료 형태를 코멘트로 남기자
+
+;[part 1]
+;주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다. 두번 나타난 문자가 있는 문자열의 수 _ 세번 나타난 문자가 있는 문자열의 수를 반환하시오. 예) abcdef 어떤 문자도 두번 혹은 세번 나타나지 않음 -> (두번 나오는 문자열 수: 0, 세번 나오는 문자열 수: 0) bababc 2개의 a, 3개의 b -> (두번 나오는 문자열 수: 1, 세번 나오는 문자열 수: 1) abbcde 2개의 b -> (두번 나오는 문자열 수: 2, 세번 나오는 문자열 수: 1) abcccd 3개의 c -> (두번 나오는 문자열 수: 2, 세번 나오는 문자열 수: 2) aabcdd 2개의 a, 2개의 d 이지만, 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 3, 세번 나오는 문자열 수: 2) abcdee 2개의 e -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 2) ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3) 답 : 4 _ 3 = 12
+(comment
+  ;; 전체가 parse?
+  (def count_map (->> input
+                      (map frequencies)
+                      (map vals)
+                      (map))) ;some 으로 바꾸고싶다
+
+  ; juxt 사용해보기
+  ; {:"}
+  (*
+    (count (filter #(% 2) count_map))
+    (count (filter #(% 3) count_map))))
+
+
+
+
+;[part 2]
+;여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.
+
+(defn get_common_letters [a b]
+  "string a, b 를 input 으로 받아 공통된 문자열을 return 하는 function"
+  (->> (map vector a b)
+       (reduce (fn [s [x y]] (if (= x y) (str s x) s)) "")))
+
+(defn print_diff [list]
+  "string sequence 를 input 으로 받아 모든 조합 중 1개의 알파벳만 다른 경우의 공통된 문자열을 print 하는 function"
+  (when (seq list)
+    (doseq [b (rest list) :let [a (first list) common_letters (get_common_letters a b)]]
+      (when (= (- (count a) 1) (count common_letters))
+        (println common_letters)))
+    (recur (rest list))))
+
+(comment
+    (print_diff input))
