b2326f53523c9c6e9a76f74547b4bac8510ab18a
Jungin Kwon <jungin.kwon1@gmail.com>
2021-07-08 17:25:37 +0900

    Change order

--- src/day_3.clj
+++ src/day_3.clj
@@ -27,18 +27,6 @@
          (str a "-" b))
        (into '())))
 
-(defn solve-part-1 [input]
-  (->> input
-       (map generate-list)
-       (apply concat)
-       frequencies
-       vals
-       (filter #(> % 1))
-       count))
-
-(comment
-  (solve-part-1 input))
-
 ;[part 1]
 ;다음과 같은 입력이 주어짐.
 ;
@@ -56,10 +44,17 @@
 ;.111133.
 ;........
 ;여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역. 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
+(defn solve-part-1 [input]
+  (->> input
+       (map generate-list)
+       (apply concat)
+       frequencies
+       vals
+       (filter #(> % 1))
+       count))
 
-
-
-
+(comment
+  (solve-part-1 input))
 
 ;[part 2]
 ;입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력. 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
