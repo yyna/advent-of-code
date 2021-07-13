31ceddd79d30a08f4a5fd7907c3e6d6c87621c0d
Jungin Kwon <jungin.kwon1@gmail.com>
2021-07-13 11:21:24 +0900

    Solve 2020 day 1 using combinatorics

--- src/aoc2020/day_1.clj
+++ src/aoc2020/day_1.clj
@@ -1,4 +1,6 @@
-(ns aoc2020.day_1 (:require [clojure.java.io :as io]))
+(ns aoc2020.day_1
+  (:require [clojure.java.io :as io])
+  (:require [clojure.math.combinatorics :as combo]))
 
 (defn parse [input]
   "multiline string input 을 정수 sequence 로 변경해 return 하는 function"
@@ -10,14 +12,9 @@
                 slurp
                 parse))
 
-(defn find-two-numbers [input n]
-  (->> (for [x (range (count input))
-             y (range (count input))
-             :when (< x y)]
-         (let [a (nth input x)
-               b (nth input y)]
-           {:a a :b b :sum (+ a b)}))
-       (filter #(= n (% :sum)))
+(defn find-numbers [input sum n]
+  (->> (combo/combinations input n)
+       (filter #(= sum (apply + %)))
        first))
 
 ;;[part 1]
@@ -25,8 +22,7 @@
 ;;
 ;;예) 1721 979 366 299 675 1456 의 경우 1721 * 299 = 514579 를 출력
 (defn solve-part-1 [input]
-  (->> (find-two-numbers input 2020)
-       ((juxt :a :b))
+  (->> (find-numbers input 2020 2)
        (apply *)))
 
 (comment
@@ -36,3 +32,9 @@
 ;;같은 입력이 주어질 때, 더해서 2020이 되는 세 숫자의 합을 구하시오.
 ;;
 ;;예) 2020 = 979 + 366 + 675, 곱하면 241861950 을 출력
+(defn solve-part-2 [input]
+  (->> (find-numbers input 2020 3)
+       (apply *)))
+
+(comment
+  (solve-part-2 input))
