7ae88597ccbbdb5b709236265051a5d6db8e123b
Jungin Kwon <jungin.kwon1@gmail.com>
2021-07-15 14:53:19 +0900

    visit 을 set 으로 관리하기 (여전히 답은 안나옴..)

--- src/aoc2020/day_8.clj
+++ src/aoc2020/day_8.clj
@@ -8,53 +8,49 @@
        (mapv (fn [line]
                (let [[operator operand] (str/split line #" ")]
                  {:operator operator
-                  :operand (Integer/parseInt operand)
-                  :visit 0})))))
-
+                  :operand (Integer/parseInt operand)})))))
 
 (def input (->> "aoc2020/day_8_input"
                 io/resource
                 slurp
                 parse))
-;({:operator acc, :operand 7, :visit 0}
-; {:operator acc, :operand 23, :visit 0}
-; {:operator acc, :operand 41, :visit 0})
+;({:operator acc :operand 7}
+; {:operator acc :operand 23}
+; {:operator acc :operand 41})
 ; ...)
 
-;(def input (->> "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\nnop -4\nacc +6"
+;(def input (->> "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
 ;                parse))
 
-(defn accumulate [input]
-  "input 된 list 를 한번 accumulate 한 후 결과를 return 하는 function"
-  (let [[list index value] ((juxt :list :index :value) input)
-        [operator operand visit] ((juxt :operator :operand :visit) (nth list index))]
-    {:list (assoc-in list [index :visit] (inc visit))
+(defn execute [input]
+  "input 된 list 를 한번 실행한 후 결과를 return 하는 function"
+  (let [[list index value visit] ((juxt :list :index :value :visit) input)
+        [operator operand] ((juxt :operator :operand) (nth list index))]
+    {:list list
      :index (mod (+ (if (= operator "jmp") (+ index operand) (inc index))) (count list))
-     :value (+ value (if (= operator "acc") operand 0))}))
+     :value (+ value (if (= operator "acc") operand 0))
+     :visit (conj visit index)}))
 
 (defn infinite? [input]
   "현재 list 의 상태가 infinite 인지 확인 하는 function, 확인할 수 없으면 nil 이 return 됨"
-  (let [[list index] ((juxt :list :index) input)
-        [operator visit] ((juxt :operator :visit) (last list))
-        duplicate-visit (count (filter #(> (% :visit) 1) list))]
+  (let [[list index visit] ((juxt :list :index :visit) input)
+        operator ((last list) :operator)]
     (cond
-      (and (= index 0) (not= operator "jmp") (= visit 1)) false
-      (> duplicate-visit 0) true)))
+      (and (not= operator "jmp") (visit (- (count list) 1))) false
+      (visit index) true)))
 
-(defn get-last-value [input]
-  "infinite 인지 확인된 상태에서의 accumulator value 및 infinite 여부를 return 하는 function"
-  (let [states (take-while #(nil? (infinite? %)) (iterate accumulate {:list input :index 0 :value 0}))
-        last-state (last states)
-        next-state (accumulate last-state)]
-    {:last-value (:value last-state)
-     :next-value (:value next-state)
-     :infinite? (infinite? next-state)}))
+(defn generate-states [input]
+  "infinite 여부를 확인하기 직전부터의 states 를 return 하는 function"
+  (->> {:list input :index 0 :value 0 :visit #{}}
+       (iterate execute)
+       (drop-while #(nil? (infinite? %)))))
 
 ;;[part 1]
 (defn solve-part-1 [input]
   (->> input
-       get-last-value
-       :last-value))
+       generate-states
+       first
+       :value))
 
 (comment
   (solve-part-1 input))
@@ -63,18 +59,22 @@
   (->> (for [i (range (count input))]
          (let [[operator operand] ((juxt :operator :operand) (nth input i))]
            (cond
-             (= operator "nop") (assoc input i {:operator "jmp" :operand operand :visit 0})
-             (= operator "jmp") (assoc input i {:operator "nop" :operand operand :visit 0}))))
+             (= operator "nop") (assoc input i {:operator "jmp" :operand operand})
+             (= operator "jmp") (assoc input i {:operator "nop" :operand operand}))))
        (filter #(seq %))))
 
 ;;[part 2]
 (defn solve-part-2 [input]
   (->> input
        generate-all-cases
-       (map get-last-value)
-       (filter #(= false (% :infinite?)))
-       last
-       :next-value))
+       (map #(->> %
+                  generate-states
+                  second))
+       (filter #(->> %
+                     infinite?
+                     (= false)))
+       first
+       :value))
 
 (comment
   (solve-part-2 input))
