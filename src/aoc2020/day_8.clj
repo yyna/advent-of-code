0332b9e4e50a33a57edefad98eb25025495c7289
Jungin Kwon <jungin.kwon1@gmail.com>
2021-07-19 10:55:01 +0900

    2020 day 8 mod 없애기

--- src/aoc2020/day_8.clj
+++ src/aoc2020/day_8.clj
@@ -19,7 +19,7 @@
 ; {:operator acc :operand 41})
 ; ...)
 
-;(def input (->> "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
+;(def input (->> "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\nnop -4\nacc +6"
 ;                parse))
 
 (defn execute [input]
@@ -27,34 +27,29 @@
   (let [{:keys [list index]} input
         {:keys [operator operand]} (nth list index)]
     (-> input
-        (update :index #(mod (+ % (if (= operator "jmp") operand 1)) (count list)))
+        (update :index #(+ % (if (= operator "jmp") operand 1)))
         (update :value #(+ % (if (= operator "acc") operand 0)))
         (update :visit-set #(conj % index)))))
 
 (defn infinite? [input]
   "현재 list 의 상태가 infinite 인지 확인 하는 function, 확인할 수 없으면 nil 이 return 됨"
-  (let [{:keys [list index visit-set]} input
-        operator (:operator (last list))]
+  (let [{:keys [list index visit-set]} input]
     (cond
-      (and (not= operator "jmp") (visit-set (- (count list) 1))) false
+      (>= index (count list)) false
       (visit-set index) true)))
-
-(defn generate-states [input]
-  "infinite 여부를 확인하기 직전부터의 states 를 return 하는 function"
-  (->> {:list input :index 0 :value 0 :visit-set #{}}
-       (iterate execute)
-       (drop-while #(nil? (infinite? %)))))
 
 ;;[part 1]
 (defn solve-part-1 [input]
-  (->> input
-       generate-states
-       first
+  (->> {:list input :index 0 :value 0 :visit-set #{}}
+       (iterate execute)
+       (take-while #(nil? (infinite? %)))
+       last
        :value))
 
 (comment
   (solve-part-1 input))
 
+;;[part 2]
 (defn generate-all-cases [input]
   (->> (for [i (range (count input))]
          (let [{:keys [operator operand]} (nth input i)]
@@ -64,16 +59,17 @@
              nil)))
        (filter #(seq %))))
 
-;;[part 2]
+(defn execute-to-infinity-or-to-the-end [input]
+  (->> {:list input :index 0 :value 0 :visit-set #{}}
+       (iterate execute)
+       (drop-while #(nil? (infinite? %)))
+       first))
+
 (defn solve-part-2 [input]
   (->> input
        generate-all-cases
-       (map #(->> %
-                  generate-states
-                  second))
-       (filter #(->> %
-                     infinite?
-                     (= false)))
+       (map #(execute-to-infinity-or-to-the-end %))
+       (filter #(= false (infinite? %)))
        first
        :value))
 
