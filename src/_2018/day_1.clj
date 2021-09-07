613e68ac1b8d60e07e2121d3c33dd287d5a123c6
Jungin Kwon <jungin.kwon1@gmail.com>
2021-09-07 23:34:45 +0900

    reductions 로 part 2 풀기

--- src/_2018/day_1.clj
+++ src/_2018/day_1.clj
@@ -50,16 +50,27 @@
      :sum-set (conj sum-set sum)
      :no-duplicate? (nil? (sum-set sum))}))
 
+(->> {:input (cycle input) :sum 0 :sum-set #{} :no-duplicate? true}
+     (iterate add)
+     (drop-while :no-duplicate?)
+     first
+     :sum)
+
+;; reductions 로 part 2 풀기
+(defn first-duplicate
+  [input]
+  (reduce (fn [set x]
+            (if (set x)
+              (reduced x)
+              (conj set x)))
+          #{} input))
+
 ;;[part 2]
 ;;주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;;
 ;;예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임. 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 (defn solve-part-2 [input]
-  (->> {:input (cycle input) :sum 0 :sum-set #{} :no-duplicate? true}
-       (iterate add)
-       (drop-while :no-duplicate?)
-       first
-       :sum))
+  (first-duplicate (reductions + (cycle input))))
 
 (comment
   (solve-part-2 input))
