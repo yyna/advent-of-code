ac7df8f2d68fdab655eef2eefe9ea6f8777ceaf9
Jungin Kwon <jungin.kwon1@gmail.com>
2021-12-09 23:24:09 +0900

    2021 Day 7: The Treachery of Whales

---
+++ src/_2021/day_7.clj
@@ -0,0 +1,33 @@
+(ns _2021.day_7
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(defn parse [path]
+  (map #(Integer/parseInt %) (-> path
+                                 io/resource
+                                 slurp
+                                 (s/split #"\,"))))
+
+(defn gauss-sum
+  [n]
+  (/ (* n (inc n)) 2))
+
+(comment
+
+  (def crabs-position (parse "2021/day_7_input"))
+
+  ;;[part 1] How much fuel must they spend to align to that position?
+  (let [min-value (apply min crabs-position)
+        max-value (apply max crabs-position)]
+    (apply min (for [number (range min-value (inc max-value))]
+                 (apply + (for [position crabs-position]
+                            (Math/abs (- number position)))))))
+
+  ;;[part 2]
+  (let [min-value (apply min crabs-position)
+        max-value (apply max crabs-position)]
+      (apply min (for [number (range min-value (inc max-value))]
+                   (apply + (for [position crabs-position]
+                              (gauss-sum (Math/abs (- number position))))))))
+
+  ,)
